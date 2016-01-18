8e371a4f08789146b529a9e63d048aeae59bb951
fmedda15 <meddii@tiscali.it>
2016-01-18 11:53:23 +0100

    Create barraMenuAmm.php

---
+++ barraMenuAmm.php
@@ -0,0 +1,54 @@
+	<div class="menu">
+	<!-- Titolo sito -->
+		<div class="titolo">
+			<a> Actors Database </a>
+		</div>
+
+<!-- Menu destra, Menu Real -->
+		<div class="menusx">
+				 <ul class="#">
+	    				<li> <a href="index.php?page=homeAmm" class="#"> Home </a>
+					<li>
+					<a href="#" class="index.php?page=homeAmm">Actors</a>
+						<ul class="mainsubnav">
+								   <li> <a href="index.php?page=viewActor" class="#">View</a></li>	    	        
+								   <li> <a href="index.php?page=addActor" class="#">Add</a></li>
+							           <li> <a href="index.php?page=deleteActor" class="#">Delete</a></li>
+							</ul>
+					</li>
+				    	<li>
+						<a href="index.php?page=homeAmm" class="#">Users</a>
+
+					    		<ul class="mainsubnav">
+								   <li> <a href="index.php?page=viewUser" class="#">View</a></li>	    	        
+								   <li> <a href="index.php?page=deleteUser" class="#">Delete</a></li>
+							</ul>
+				    	</li>
+					<li>
+						<a href="#" class="index.php?page=homeAmm">Comments</a>
+
+					    		<ul class="mainsubnav">
+								   <li> <a href="index.php?page=viewComments" class="#">View</a></li>	    	        
+								   <li> <a href="index.php?page=addCommentAmm" class="#">Add</a></li>
+								   <li> <a href="index.php?page=deleteComment" class="#">Delete</a></li>
+							</ul>
+				    	</li>
+				    	
+				</ul>
+		</div>
+	 
+    
+	<!-- Menu sinistra, LOGIN -->	
+		<div class="menudx">
+			    <ul class="#">
+			      
+				<li>
+					<a href="index.php?page=logout" class="account">Logout</a>
+					
+				</li>
+			    
+			    </ul>
+		</div>
+	</div>
+<div class="sottomenu">
+</div>
