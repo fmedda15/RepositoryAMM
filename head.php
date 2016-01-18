029fc303f0fcfae9df7c91cc0bac6fcb40e6747d
fmedda15 <meddii@tiscali.it>
2016-01-18 11:55:45 +0100

    Create head.php

---
+++ head.php
@@ -0,0 +1,10 @@
+	<head>
+		<title>ActorsDB</title>
+		<meta charset="utf-8" />
+		<meta name="keywords" content="Actors, DB, DataBase, Site, Amm, 2015" />
+		<meta name="description" content="Progetto AMM 2015" />
+		<meta name="author" content="Federico Medda" />
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="css/cssGenerale.css" />
+		<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+	</head>
