10ec32ee34bfa31606456670bf6dad93e137729a
fmedda15 <meddii@tiscali.it>
2016-01-18 12:13:41 +0100

    Create homeDefault.php

---
+++ view/default/homeDefault.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<?php include_once('inc/head.php');?>
+	<body>
+		<?php include_once('inc/barraMenuDefault.php');?>
+		
+		<div class="contenuto">
+		<p> welcome <br><br></p>
+		</div>
+		
+	
+	</body>
+	<?php include_once('inc/footer.php');?>
+	
+</html>
