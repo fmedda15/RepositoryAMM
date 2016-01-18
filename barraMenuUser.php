9e65637ba1b7967b456d9ceee452ce99bef900aa
fmedda15 <meddii@tiscali.it>
2016-01-18 11:54:50 +0100

    Create barraMenuUser.php

---
+++ barraMenuUser.php
@@ -0,0 +1,41 @@
+	<div class="menu">
+	<!-- Titolo sito -->
+		<div class="titolo">
+			<a> Actors Database </a>
+		</div>
+<!-- Menu destra, Menu Real -->
+		<div class="menusx">
+				 <ul class="#">
+	    				<li> <a href="index.php?page=homeUser" class="#"> Home </a>
+					<li> <a href="index.php?page=viewActorsUser" class="#"> Actors </a>	
+				    	<li>
+						<a href="index.php?page=homeUser" class="#">Comments</a>
+
+					    		<ul class="mainsubnav">
+								   <li> <a href="index.php?page=viewCommentsUser" class="#">View</a></li>	    	        
+								   <li> <a href="index.php?page=addCommentUser" class="#">Add</a></li>
+							</ul>
+				    	</li>
+				    	
+				</ul>
+		</div>
+	 
+    
+	<!-- Menu sinistra, LOGIN -->	
+		<div class="menudx">
+			    <ul class="#">
+			      
+				<li>
+					<a href="index.php?page=homeUser" class="account">Account</a>
+						<ul class="mainsubnav">
+								   <li> <a href="index.php?page=profile" class="#">View</a></li>	    	        
+								   <li> <a href="index.php?page=settings" class="#">Settings</a></li>
+							           <li> <a href="index.php?page=logout" class="#">Logout</a></li>
+							</ul>
+				</li>
+			    
+			    </ul>
+		</div>
+	</div>
+<div class="sottomenu">
+</div>
