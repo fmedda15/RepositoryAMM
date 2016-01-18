719efca44e80cc9ee39cbfeb2114bee28574a4f2
fmedda15 <meddii@tiscali.it>
2016-01-18 12:16:04 +0100

    Create addCommentUser.php

---
+++ view/user/addCommentUser.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<?php include_once('inc/head.php');?>
+	
+	<body>
+		<?php include_once('inc/barraMenuUser.php');?>
+	
+		<div class="contenuto">
+			<p> Insert New Comment<br><br></p>
+				<form id="login" action="index.php?page=addCommentNowUser" method="POST">
+					<textarea name="testo" rows="5" cols="40" onclick="this.value='';">Insert your comment</textarea><br>
+					<button id="bottom" name="add">Add</button>
+				</form> 
+		</div>
+	
+	</body>
+	<?php include_once('inc/footer.php');?>
+</html>
