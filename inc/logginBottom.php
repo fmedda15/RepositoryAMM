565c3593818d89c3117a45d93546f7fd05a8e0cd
fmedda15 <meddii@tiscali.it>
2016-01-18 11:57:12 +0100

    Create logginBottom.php

---
+++ inc/logginBottom.php
@@ -0,0 +1,11 @@
+<!-- Form login -->
+<form id="login" action="index.php?page=login" method="POST">
+	<input type="text" name="nome" id="text" value="" required="required">
+	<br><input type="password" name="password" id="text" required="required">
+	<br><button id="bottom">Login</button>
+</form> 
+	
+<!-- Form registrazione nuovo utente-->
+<form id="login" action="index.php?page=registra" method="POST">
+	<br><button id="bottom">Sign in</button>
+</form> 
