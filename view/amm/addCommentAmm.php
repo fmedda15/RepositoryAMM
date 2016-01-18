bc1ced40465f8ffb2e685b50ff9f3f602a231fe0
fmedda15 <meddii@tiscali.it>
2016-01-18 12:09:22 +0100

    Create addCommentAmm.php

---
+++ view/amm/addCommentAmm.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<?php include_once('inc/head.php');?>
+	
+	<body>
+		<?php include_once('inc/barraMenuAmm.php');?>
+	
+		<div class="contenuto">
+			<p> Insert New Comment <br><br></p>
+				<form id="login" action="index.php?page=addNowComment" method="POST">
+					<input type="text" name="id" id="text" value="id" required="required" onclick="this.value='';"><br>
+					<textarea name="testo" rows="5" cols="40" onclick="this.value='';">Films</textarea><br>
+					<button id="bottom" name="add">Add</button>
+				</form> 
+		</div>
+	
+	</body>
+	<?php include_once('inc/footer.php');?>
+</html>
