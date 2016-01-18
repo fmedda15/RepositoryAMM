d668f4d41a92c9593c56b39ceb2596df53bf7453
fmedda15 <meddii@tiscali.it>
2016-01-18 12:11:15 +0100

    Create homeAmm.php

---
+++ view/amm/homeAmm.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<?php include_once('inc/head.php');?>
+	
+	<body>
+		<?php include_once('inc/barraMenuAmm.php');?>
+	
+		<div class="contenuto"> 
+
+			<p> Sei loggato come Amministratore della pagina <br><br></p>
+
+		</div>
+	
+	</body>
+	<?php include_once('inc/footer.php');?>
+</html>
