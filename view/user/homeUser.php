637bb4cbef5e84a50dc4f1e3e88d13299009036a
fmedda15 <meddii@tiscali.it>
2016-01-18 12:16:32 +0100

    Create homeUser.php

---
+++ view/user/homeUser.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+	<?php include_once('inc/head.php');?>
+	
+	<body>
+		<?php include_once('inc/barraMenuUser.php');?>
+		<div class="contenuto">
+		
+			<p> Accesso effettuato con successo <br><br></p>			
+
+		</div>
+	</body>
+	<!---Footer--->
+<?php include_once('inc/footer.php');?>
+</html>
