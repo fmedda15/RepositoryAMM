ca14d3266a4d68af20b2062491019836454c75f7
fmedda15 <meddii@tiscali.it>
2016-01-18 12:13:18 +0100

    Create comments.php

---
+++ view/default/comments.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<?php include_once('inc/head.php');?>
+	
+	<body>
+		<?php include_once('inc/barraMenuDefault.php');?>
+	
+		<div class="contenuto"> 
+			<p> Comments <br><br></p>
+		
+				<?php
+
+				$query = $this->modelSession->getComments(); //richiamo la getactors() dal modelSession
+				
+				?>
+				
+
+		</div>
+	
+	</body>
+	<?php include_once('inc/footer.php');?>
+</html>
