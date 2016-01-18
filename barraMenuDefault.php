f1e16165cc20e125a2dbf3db8841460021279f03
fmedda15 <meddii@tiscali.it>
2016-01-18 11:54:18 +0100

    Create barraMenuDefault.php

---
+++ barraMenuDefault.php
@@ -0,0 +1,27 @@
+	<div class="menu">
+	<!-- Titolo sito -->
+		<div class="titolo">
+			<a> Actors Database </a>
+		</div>
+	<!-- Menu destra, Menu Real -->
+		<div class="menusx">
+				 <ul class="#">
+	    				<li> <a href="index.php?page=homeDefault" class="#"> Home </a> </li>
+					<li> <a href="index.php?page=actors" class="#"> Actors </a> </li>
+					<li> <a href="index.php?page=comments" class="#"> Comments </a> </li>		    	
+				</ul>
+		</div>
+	 
+    
+	<!-- Menu sinistra, LOGIN -->	
+		<div class="menudx">
+			    <ul class="#">        
+				<li> <a href="index.php?page=login" class="#">Login</a> </li>
+				<li> <a href="index.php?page=signIn" class="#">Sign In</a> </li>		
+			    </ul>
+		</div>
+	</div>
+
+<div class="sottomenu">
+
+</div>
