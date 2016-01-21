6cf546979e4e4b77eb8c616df9e6dc114949756f
fmedda15 <meddii@tiscali.it>
2016-01-21 17:23:27 +0100

    Update settingsUser.php

--- view/user/settingsUser.php
+++ view/user/settingsUser.php
@@ -9,13 +9,11 @@
 		<div class="contenuto">
 			- <p> Settings <br><br></p>
 				<form id="setting" action="index.php?page=settingUser" method="POST">
-					<input type="text" name="newNome" id="textLogin" value="Insert a new User" required="required"><br><br>
-					<input type="text" name="newPassword" id="textLogin" value="Insert a new Password" required="required">
+					<input type="text" name="newNome" id="textLogin" value="Insert a new User" required="required"  onclick="this.value='';"><br><br>
+					<input type="text" name="newPassword" id="textLogin" value="Insert a new Password" required="required"  onclick="this.value='';">
 					<br><br><button id="bottomLogin">Change</button>
 				</form> 
 		
 		</div>
 	</body>
-	<!---Footer--->
-	<?php include_once('inc/footer.php');?>
 </html>
