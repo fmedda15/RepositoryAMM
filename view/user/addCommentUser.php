d7aa7e268fb5b25d324eefa4b43174a6ba0ca0c6
fmedda15 <meddii@tiscali.it>
2016-01-21 17:22:28 +0100

    Update addCommentUser.php

--- view/user/addCommentUser.php
+++ view/user/addCommentUser.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html>
+
 <html>
 	<?php include_once('inc/head.php');?>
 	
 	<body>
 		<?php include_once('inc/barraMenuUser.php');?>
-	
+
 		<div class="contenuto">
-			<p> Insert New Comment<br><br></p>
-				<form id="login" action="index.php?page=addCommentNowUser" method="POST">
-					<textarea name="testo" rows="5" cols="40" onclick="this.value='';">Insert your comment</textarea><br>
-					<button id="bottom" name="add">Add</button>
-				</form> 
+		<p> Change Settings<br><br> </p>
+			<form id="login" action="index.php?page=change" method="POST">
+				<input type="text" name="nickname" id="text" value="New NickName" required="required" onclick="this.value='';"><br>
+				<input type="text" name="password" id="text" value="New Password" required="required" onclick="this.value='';"><br>
+				<input type="text" name="nome" id="text" value="New Name" required="required" onclick="this.value='';"><br>
+				<input type="text" name="cognome" id="text" value="New Prename" required="required" onclick="this.value='';"><br>
+				<input type="text" name="email" id="text" value="New Email" required="required" onclick="this.value='';"><br>
+				<button id="bottom" name="signIn">Change</button>
+			</form> 
 		</div>
-	
+		
+		<br>
 	</body>
 </html>
