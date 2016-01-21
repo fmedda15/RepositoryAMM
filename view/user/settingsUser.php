1fae477349ef70477e4fff88f8f9193243ca4235
fmedda15 <meddii@tiscali.it>
2016-01-21 17:23:43 +0100

    Update settingsUser.php

--- view/user/settingsUser.php
+++ view/user/settingsUser.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
-
 <html>
 	<?php include_once('inc/head.php');?>
 	
 	<body>
 		<?php include_once('inc/barraMenuUser.php');?>
 	
-		<div class="contenuto">
-			- <p> Settings <br><br></p>
-				<form id="setting" action="index.php?page=settingUser" method="POST">
-					<input type="text" name="newNome" id="textLogin" value="Insert a new User" required="required"  onclick="this.value='';"><br><br>
-					<input type="text" name="newPassword" id="textLogin" value="Insert a new Password" required="required"  onclick="this.value='';">
-					<br><br><button id="bottomLogin">Change</button>
-				</form> 
-		
+		<div class="contenuto"> 
+			<p> Actors <br><br> </p>
+	
+			<?php
+
+				$query = $this->modelUser->viewActorsUser(); //richiamo la viewComments() dal modelSession
+				
+			?>
 		</div>
+	
 	</body>
 </html>
