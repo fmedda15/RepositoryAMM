1698ad707ba06fecaf100e040bbc2f8c2bd06fa0
fmedda15 <meddii@tiscali.it>
2016-01-21 17:20:03 +0100

    Update problems.php

--- view/default/problems.php
+++ view/default/problems.php
@@ -8,13 +8,13 @@
 		
 		<!-- Form per segnalare problemi -->
 		<div class="contenuto">
-		<p> Report Problems<br><br> </p>
+			<p> Reports Problems<br><br> </p>
 			<form id="login" action="index.php?page=sendProblems" method="POST">
-				<textarea name="newsomething" rows="8" cols="100" required="required"></textarea><br><br>
+				<textarea name="problems" id="problems" rows="8" cols="100" required="required"  onclick="this.value='';"></textarea><br><br>
 				<br><button id="bottom">Send</button>
 			</form> 
 		</div>
-		<br>
+	<br>
 	</body>
 	<?php include_once('inc/footer.php');?>
 </html>
