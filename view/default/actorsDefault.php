f395e14ef0261ad288724e60230ab8fa1f88f434
fmedda15 <meddii@tiscali.it>
2016-01-21 17:18:20 +0100

    Update actorsDefault.php

--- view/default/actorsDefault.php
+++ view/default/actorsDefault.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html>
 <html>
 	<?php include_once('inc/head.php');?>
+
 	
 	<body>
 		<?php include_once('inc/barraMenuDefault.php');?>
 	
 		<div class="contenuto"> 
-			<p> View Actors <br><br> </p>
+			<p> Actors <br><br> </p>
 				<?php
+				
 
 				$query = $this->modelSession->getActors(); //richiamo la getactors() dal modelSession
-				
+	
 				?>
 				
 
 		</div>
 	
 	</body>
-	<?php include_once('inc/footer.php');?>
 </html>
